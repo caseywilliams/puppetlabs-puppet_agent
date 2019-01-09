168d8329ad77275113d1b37e7f63c3b33bc1635d
Casey Williams <caseywilliams@users.noreply.github.com>
2019-01-09 09:57:37 -0800

    (MODULES-8445) Fix collection determination outside PE
    Previously, the name of the puppet collection was determined based on
    the installed version of PE, which was correct for PE installations, but
    for open source users, it incorrectly selected 'PC1' as the collection
    name regardless of which version of the agent was installed.
    
    This derives the collection name from the puppet-agent package version
    (`aio_agent_version` fact) instead, as follows:
    
    - Agents in the 1.y.z series (containing puppet 4) still get `PC1`
    - Agents in the 5.y.z series (containing puppet 5) now get `puppet5`
    - Agents in the 6.y.z series (containing puppet 6) now get `puppet6`
    - Anything else will get 'puppet', which always defaults to the latest
      release (this would be puppet6 right now)

--- spec/classes/puppet_agent_params_spec.rb
+++ spec/classes/puppet_agent_params_spec.rb
@@ -1,77 +1,63 @@
 require 'spec_helper'
 
 describe 'puppet_agent::params' do
-  let(:facts) do
-    {
-      is_pe:           true,
-      clientversion:   '5.5.3',
-      osfamily:        'Debian',
-      operatingsystem: 'Debian',
-      servername:      'server',
-      # custom fact meant to be used only for tests in this file
-      custom_fact__pe_version: '2018.1.3'
-    }
-  end
+  facts = {
+    osfamily: 'Debian'
+  }
 
-  before(:each) do
-    Puppet::Parser::Functions.newfunction(:pe_build_version, type: :rvalue, doc: '') do |args|
-      lookupvar('::custom_fact__pe_version')
-    end
-  end
-
-  context 'collection' do
-    # rspec-puppet lets us query the compiled catalog only, so we can only check if any specific resources
-    # have been declared. We cannot query for a class' variables, so we cannot query for the collection
-    # variable's value. But we can use a workaround by creating a notify resource whose message contains
-    # the value and query that instead since it will be added as part of the catalog. post_condition tells
-    # rspec-puppet to include this resource only after our class has been compiled, which is what we want.
-    let(:notify_title) { "check puppet_agent::params::collection's value" }
-    let(:post_condition) do
-      <<-NOTIFY_RESOURCE
+  # rspec-puppet lets us query the compiled catalog only, so we can only check
+  # if any specific resources have been declared. We cannot query for class
+  # variables, so we cannot query for the collection variable's value. But we
+  # can use a workaround by creating a notify resource whose message contains
+  # the value and query that instead since it will be added as part of the
+  # catalog. notify_resource tells rspec-puppet to include this resource only
+  # after our class has been compiled, which is what we want.
+  let(:notify_title) { "check puppet_agent::params::collection's value" }
+  let(:post_condition) do
+    <<-NOTIFY_RESOURCE
 notify { "#{notify_title}":
   message => "${::puppet_agent::params::collection}"
 }
-      NOTIFY_RESOURCE
-    end
+    NOTIFY_RESOURCE
+  end
 
-    def sets_collection_to(collection)
-      is_expected.to contain_notify(notify_title).with_message(collection)
-    end
+  def sets_collection_to(collection)
+    is_expected.to contain_notify(notify_title).with_message(collection)
+  end
 
-    context 'not in PE' do
-      let(:facts) { super().merge(is_pe: false, custom_fact__pe_version: '') }
+  context 'with a puppet-agent package from the 1.y.z series (contains puppet 4)' do
+    let(:facts) {
+      facts.merge(
+        aio_agent_version: '1.10.14'
+      )
+    }
+    it { sets_collection_to('PC1') }
+  end
 
-      it { sets_collection_to('PC1') }
-    end
+  context 'with a puppet-agent package from the 5.y.z series (contains puppet 5)' do
+    let(:facts) {
+      facts.merge(
+          aio_agent_version: '5.5.9'
+      )
+    }
+    it { sets_collection_to('puppet5') }
+  end
 
-    context 'pe_version < 2018.1.3' do
-      let(:facts) { super().merge(custom_fact__pe_version: '2018.1.2') }
+  context 'with a pre-release package of puppet 6 (contains puppet 6)' do
+    let(:facts) {
+      facts.merge(
+        aio_agent_version: '5.99.0'
+      )
+    }
+    it { sets_collection_to('puppet6') }
+  end
 
-      it { sets_collection_to('PC1') }
-    end
-
-    context 'pe_version == 2018.1.3' do
-      let(:facts) { super().merge(custom_fact__pe_version: '2018.1.3') }
-
-      it { sets_collection_to('puppet5') }
-    end
-
-    context '2018.1.3 < pe_version < 2018.2' do
-      let(:facts) { super().merge(custom_fact__pe_version: '2018.1.5') }
-
-      it { sets_collection_to('puppet5') }
-    end
-
-    context 'pe_version == 2018.2' do
-      let(:facts) { super().merge(custom_fact__pe_version: '2018.2') }
-
-      it { sets_collection_to('puppet6') }
-    end
-
-    context 'pe_version > 2018.2' do
-      let(:facts) { super().merge(custom_fact__pe_version: '2018.3') }
-
-      it { sets_collection_to('puppet6') }
-    end
+  context 'with a puppet-agent package from the 6.y.z series (contains puppet 6)' do
+    let(:facts) {
+      facts.merge(
+        aio_agent_version: '6.0.2'
+      )
+    }
+    it { sets_collection_to('puppet6') }
   end
 end
